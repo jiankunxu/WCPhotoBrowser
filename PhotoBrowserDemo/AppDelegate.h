b46dff14818f5b33d74e256ef19fe5e3c668e7d0
wangchao <504615732@qq.com>
2017-11-28 09:45:15 +0800

    Initial Commit

---
+++ PhotoBrowserDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoBrowserDemo
+//
+//  Created by 王超 on 2017/11/28.
+//  Copyright © 2017年 王超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoBrowserDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoBrowserDemo
+//
+//  Created by 王超 on 2017/11/28.
+//  Copyright © 2017年 王超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
